7ef61940c7531e8ddaf5ca14398b7a2de18beaf7
Akhildh <callmeaki09@gmail.com>
2021-04-23 18:29:44 +0530

    add you google map api key here

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -56,5 +56,5 @@ export class MapContainer extends Component {
   }
 
   export default GoogleApiWrapper({
-    apiKey: process.env.REACT_APP_API_KEY
+         apiKey: (ADD_YOUR_GOOGLE_mAP_API_KEY_HERE)
   })(MapContainer)
