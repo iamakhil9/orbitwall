7d85a1c5097d6cf47674ebba1593fb7ccf470910
Akhildh <callmeaki09@gmail.com>
2021-04-23 18:32:53 +0530

    ADD_YOUR_GOOGLE_MAP_API_KEY_HERE

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -56,5 +56,5 @@ export class MapContainer extends Component {
   }
 
   export default GoogleApiWrapper({
-         apiKey: (ADD_YOUR_GOOGLE_mAP_API_KEY_HERE)
+         apiKey: (ADD_YOUR_GOOGLE_MAP_API_KEY_HERE)
   })(MapContainer)
